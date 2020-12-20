bd02cdbe9740364605fd15883f3f68a5e8115154
Ufere Kalu <dekalusha@gmail.com>
2020-12-20 17:25:38 -0600

    added server file

---
+++ api/routes.js
@@ -0,0 +1,40 @@
+const allShows = require("./data/tvShows.json");
+const allMovies = require("./data/movies.json");
+const config = require("./config");
+
+const express = require("express");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+const path = require("path");
+
+const router = express.Router();
+
+const authCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${config.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+    audience: [config.AUDIENCE],
+    issuer: `https://${config.AUTH0_DOMAIN}/`,
+    algorithms: ["RS256"]
+});
+
+router.get("/", (req, res)  => {
+    res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
+router.get("/api/", (req, res) => {
+    res.send("API is working");
+});
+
+router.get("/api/data/tvshows", (req, res) => {
+    res.json(allShows);
+});
+
+router.get("/api/data/movies", authCheck, (req, res) => {
+    res.json(allMovies);
+})
+
+module.exports = router;
